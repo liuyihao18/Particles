e230373b0b5b02a4d2a3b7c1391198aedde0245c
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-07 15:13:03 +0800

    update

--- Particles/morton.cuh
+++ Particles/morton.cuh
@@ -4,12 +4,12 @@
 
 #pragma once
 
-#include <crt/host_defines.h>
+#include <cuda_runtime.h>
 
-using uint32_t = unsigned int;
+using uint = unsigned int;
 
 // "Insert" a 0 bit after each of the 16 low bits of x
-__forceinline__ __device__ uint32_t Part1By1(uint32_t x) {
+__forceinline__ __device__ uint Part1By1(uint x) {
 	x &= 0x0000ffff;					// x = ---- ---- ---- ---- fedc ba98 7654 3210
 	x = (x ^ (x << 8)) & 0x00ff00ff;	// x = ---- ---- fedc ba98 ---- ---- 7654 3210
 	x = (x ^ (x << 4)) & 0x0f0f0f0f;	// x = ---- fedc ---- ba98 ---- 7654 ---- 3210
@@ -19,7 +19,7 @@ __forceinline__ __device__ uint32_t Part1By1(uint32_t x) {
 }
 
 // "Insert" two 0 bits after each of the 10 low bits of x
-__forceinline__ __device__ uint32_t Part1By2(uint32_t x)
+__forceinline__ __device__ uint Part1By2(uint x)
 {
 	x &= 0x000003ff;					// x = ---- ---- ---- ---- ---- --98 7654 3210
 	x = (x ^ (x << 16)) & 0xff0000ff;	// x = ---- --98 ---- ---- ---- ---- 7654 3210
@@ -29,12 +29,12 @@ __forceinline__ __device__ uint32_t Part1By2(uint32_t x)
 	return x;
 }
 
-__forceinline__ __device__ uint32_t EncodeMorton2(uint32_t x, uint32_t y)
+__forceinline__ __device__ uint EncodeMorton2(uint x, uint y)
 {
 	return (Part1By1(y) << 1) + Part1By1(x);
 }
 
-__forceinline__ __device__ uint32_t EncodeMorton3(uint32_t x, uint32_t y, uint32_t z)
+__forceinline__ __device__ uint EncodeMorton3(uint x, uint y, uint z)
 {
 	return (Part1By2(z) << 2) + (Part1By2(y) << 1) + Part1By2(x);
 }
