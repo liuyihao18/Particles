8d76a60d087794bfb97749b499974738dc1bfb80
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-09 16:20:27 +0800

    update

--- Particles/config.h
+++ Particles/config.h
@@ -2,11 +2,11 @@
 
 constexpr float FPS = 60.0f;
 
-constexpr int PARTICLE_NUM = 9;
+constexpr int PARTICLE_NUM = 5 * 5 * 5;
 
 constexpr float DECAY = 0.9999f;
 constexpr float GRAVITY = 0.2f;
-constexpr float SPRING = 100.0f;
+constexpr float SPRING = 222.0f;
 constexpr float DAMPING = 8.0f;
 constexpr float FRICTION = 0.05f;
 
