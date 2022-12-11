1d478bdacf6747a721c77f71d7463b39a2eeb35c
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2022-12-11 10:38:21 +0800

    update

--- Particles/config.h
+++ Particles/config.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "constant.h"
 
 constexpr float FPS = 60.0f;
 
@@ -10,10 +11,11 @@ constexpr float SPRING = 222.0f;
 constexpr float DAMPING = 8.0f;
 constexpr float FRICTION = 0.05f;
 
-constexpr float ELASTICITY[4][4] = {
-	0.9f, 0.9f, 0.7f, 0.8f,
-	0.9f, 0.9f, 0.7f, 0.8f,
-	0.7f, 0.7f, 0.4f, 0.5f,
-	0.8f, 0.8f, 0.5f, 0.6f,
+constexpr float ELASTICITY[PROTO_NUM][PROTO_NUM] = {
+	0.9f, 0.9f, 0.7f, 0.8f, 0.6f,
+	0.9f, 0.9f, 0.7f, 0.8f, 0.5f,
+	0.7f, 0.7f, 0.4f, 0.5f, 0.8f,
+	0.8f, 0.8f, 0.5f, 0.6f, 0.9f,
+	0.5f, 0.9f, 0.6f, 0.7f, 0.8f
 };
 
